4b090e807ffd326e73ef4dcc314a93bc88c3fd0d
abhay <abhay2001sri@gmail.com>
2024-08-04 20:44:01 +0530

    chore: minor changes

--- week-1-website/main.js
+++ week-1-website/main.js
@@ -1,2 +1,9 @@
-var a = 0;
-console.log(a);
+let name="John";
+const age=30;
+var isStudent = true;
+let b = 'bay';
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(b);
