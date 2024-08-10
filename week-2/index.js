9170de7b6b3f7b468b71fe72c3e1e6ac867c7e0b
abhay <abhay2001sri@gmail.com>
2024-08-10 21:22:46 +0530

    async wip

--- week-2/index.js
+++ week-2/index.js
@@ -1,12 +1,22 @@
 // 1. I/O tasks 
 // assignment  - try to create a promisified version of setimeout, fetch, fs.readfile
 
-function sum(n){
-   let sum = 0;
-   for(let i=0;i<=n;i++){
-    sum+=i
-   }
-   return sum;
+const { log } = require("console");
+const fs = require("fs");
+
+// const contentsA = fs.readFileSync("a.txt", 'utf-8');
+// console.log(contentsA);
+
+// const contentsB = fs.readFileSync("b.txt", 'utf-8');
+// console.log(contentsB);
+console.log("Done! 1");
+function print(err, data){
+    console.log(data);
 }
 
-console.log(sum(5));
+
+fs.readFile("a.txt", 'utf-8', print) // asynchronously
+
+fs.readFile("b.txt", 'utf-8', print) // asynchronously
+
+console.log("Done! 2");
