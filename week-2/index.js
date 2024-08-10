d7c1f522c0e56a609d0c335d8728db2bbcdf0eb4
abhay <abhay2001sri@gmail.com>
2024-08-10 20:18:52 +0530

    using for loop

--- week-2/index.js
+++ week-2/index.js
@@ -1,7 +1,12 @@
 // 1. I/O tasks 
+// assignment  - try to create a promisified version of setimeout, fetch, fs.readfile
 
 function sum(n){
-    return n*(n+1)/2
+   let sum = 0;
+   for(let i=0;i<=n;i++){
+    sum+=i
+   }
+   return sum;
 }
 
 console.log(sum(5));
