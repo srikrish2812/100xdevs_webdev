9fa20541e04b72af82b9a23c8355994d0e920a6d
abhay <abhay2001sri@gmail.com>
2024-08-11 20:58:06 +0530

    promise wip

---
+++ week-2/promiseClass.js
@@ -0,0 +1,8 @@
+// promise class gives u a promise that it will return u something in the future
+// setTimeout promises that it runs the function after delay ms.
+
+
+function logName(){
+    console.log('');
+}
+
