912e693d2564c818387c1eef618e7d9f2031dd0e
abhay <abhay2001sri@gmail.com>
2024-08-11 21:33:11 +0530

    promise wip

--- week-2/promiseClass.js
+++ week-2/promiseClass.js
@@ -1,8 +1,22 @@
 // promise class gives u a promise that it will return u something in the future
 // setTimeout promises that it runs the function after delay ms.
+// Promises are a syntactically superior way of writing callback
 
-
-function logName(){
-    console.log('');
+function setTimeoutPromisified(ms){
+    return new Promise((resolve)=> setTimeout(resolve, ms));
 }
 
+
+function callback(){
+    console.log("sometime has passed");
+}
+
+let p = setTimeoutPromisified(3000);
+console.log(p);
+
+function random(){
+
+}
+
+let q = new Promise(random);
+console.log(q);
